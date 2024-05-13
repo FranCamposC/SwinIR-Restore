b7da4be35d58c9d0fa83788b0de3123a3cb56053
alblopnov <a.lopezbenjumea@gmail.com>
2024-05-13 12:29:17 +0200

    implementacion de calculo de tiempo de ejecución

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import subprocess
 from flask import Flask, request, redirect, url_for, render_template
 import time
 import shutil
+from datetime import datetime, timedelta
 
 
 app = Flask(__name__)
@@ -23,12 +24,17 @@ def upload_file():
             'testsets/RealSRSet+5images', 
             'static/results/swinir_real_sr_x4'
             ]
+    processing_time = None
+
     if request.method == 'POST':
         limpiar_carpetas(carpetas)
         file = request.files['file']
         if file:
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(filepath)
+
+            # Iniciar el cronómetro
+            start_time = datetime.now()
 
             subprocess.run([
                 'python', 'main_test_swinir.py',
@@ -39,8 +45,13 @@ def upload_file():
                 '--folder_gt', 'testsets/RealSRSet+5images',
                 '--tile', '400'
             ], check=True)
-            return redirect(url_for('uploaded_file', filename=file.filename))  
-    return render_template('index.html')
+
+            # Terminar el cronómetro
+            end_time = datetime.now()
+            processing_time = round((end_time - start_time).total_seconds(),2)
+
+            return redirect(url_for('uploaded_file', filename=file.filename, time=processing_time))
+    return render_template('index.html', processing_time=processing_time)
 
 @app.route('/show/<filename>')
 def uploaded_file(filename):
@@ -48,7 +59,8 @@ def uploaded_file(filename):
     processed_filename = filename.rsplit('.', 1)[0] + '_SwinIR.png'
     processed_filepath = os.path.join(app.config['RESULT_FOLDER'], processed_filename).replace('\\', '/')
     original_filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename).replace('\\', '/')
-    return render_template('index.html', processed_image=processed_filepath, original_image=original_filepath)
+    processing_time = request.args.get('time', None)
+    return render_template('index.html', processed_image=processed_filepath, original_image=original_filepath, processing_time=processing_time)
 
 
 
